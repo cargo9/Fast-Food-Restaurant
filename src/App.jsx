ac9bfb2a583b4e663ac6d2a839b96046c7257b3d
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-12-31 18:02:26 +0200

    +basic-css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const imageUrl =
     "https://mistokyia.ua/wp-content/uploads/2025/02/pizza-day-vidkryla-81-novyi-zaklad-1130x650.jpg";
     const pizzaFourChease = "https://uk.ooni.com/cdn/shop/articles/FourCheese_Resized.jpg?crop=center&height=800&v=1598453116&width=800";
+    const pizzaPhiladeph = "https://www.unileverfoodsolutions.co.id/dam/global-ufs/mcos/SEA/calcmenu/recipes/ID-recipes/sacks/Margherita-Pizza/Pizza%20Margaretha%20(Recipe%20Page).jpg"
   const productPrice = 10.99;
+  const pricePhiladelph = 7;
 
   const menuDishes = [
     { id: "id-1", name: "Pizza" },
@@ -18,7 +20,7 @@ function App() {
   // Компоненти
   const Product = ({ imgUrl, name, price, quantity }) => (
     <div>
-      <img src={imgUrl} alt={name} width="640" />
+      <img src={imgUrl} alt={name} width="640" className="product_img"/>
       <h2>{name}</h2>
       <p>Price: {price}$</p>
       <h3>Quantity: {quantity < 20 ? "Few left" : "In stock"}</h3>
@@ -38,46 +40,70 @@ function App() {
   );
 
   const MenuDishes = ({ dishes }) => (
-    <ul>
+    <ul className="menu_dishes">
       {dishes.map((dish) => (
-        <li key={dish.id}>{dish.name}</li>
+        <li className="menu_li" key={dish.id}>{dish.name}</li>
       ))}
     </ul>
   );
 
   return (
 
+    
     <div className="App">
-      <h1>Fast Food Restaurant</h1>
+
+       <section className="section_header">
+        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restaurant-logo-design-template-80b1da7e7f6651ca3c774a5fec4089bb_screen.jpg?ts=1625576767" alt="logo" className="logo_header"/>
+        <MenuDishes dishes={menuDishes} />
+      </section>
+
+      <h1 className="main_title">Fast Food Restaurant</h1>
 
       <section>
-        <img src="https://vsn.ua/storage/news/ex7zjikzDUskXZbSMVBSomCIHcExa4OF3d3V6OkK.png" alt="" />
-        <h2>Welcome to our fast food restaurant!</h2>
-          <p>We serve fresh, tasty, and high-quality food made for people who love great flavor and quick service. Our menu includes juicy burgers, crispy fries, wraps, hot dogs, and refreshing drinks — all prepared with carefully selected ingredients.</p>
-          <p>We believe fast food can be delicious and satisfying. Whether you are grabbing a quick lunch, meeting friends, or ordering takeaway, we make sure every meal is fast, affordable, and full of taste.</p>
-          <p>Come hungry, leave happy — your favorite fast food is waiting for you!</p>
+        <img src="https://vsn.ua/storage/news/ex7zjikzDUskXZbSMVBSomCIHcExa4OF3d3V6OkK.png" alt="" className="photo_restorant"/>
+        <h2 className="welcome_title">Welcome to our fast food restaurant!</h2>
+          <p className="welcome_text">We serve fresh, tasty, and high-quality food made for people who love great flavor and quick service. Our menu includes juicy burgers, crispy fries, wraps, hot dogs, and refreshing drinks — all prepared with carefully selected ingredients.</p>
+          <p className="welcome_text">We believe fast food can be delicious and satisfying. Whether you are grabbing a quick lunch, meeting friends, or ordering takeaway, we make sure every meal is fast, affordable, and full of taste.</p>
+          <p className="welcome_text">Come hungry, leave happy — your favorite fast food is waiting for you!</p>
       </section>
 
       {/* Приклад елемента продукту */}
       <section>
+        
         <h2>Pizza</h2>
+        <div className="pizza_list">
         <div>
           <img src={imageUrl} alt="Pizza" width="640" />
           <h2>Pizza Peperoni</h2>
           <p>Price: {productPrice}$</p>
           <button type="button">Add to cart</button>
         </div>
+        <div>
+          <img src={pizzaPhiladeph} alt="Pizza" width="640" />
+          <h2>Pizza Peperoni</h2>
+          <p>Price: {productPrice}$</p>
+          <button type="button">Add to cart</button>
+        </div>
+        </div>
       </section>
 
       {/* Компонент Product з умовним рендером */}
       <section>
         <h2>Pizza</h2>
+        <div className="products_list">
         <Product
           imgUrl={pizzaFourChease}
           name="Pizza Four Chease"
           price={productPrice}
           quantity={15}
         />
+        <Product
+          imgUrl={pizzaPhiladeph}
+          name="Pizza Four Chease"
+          price={pricePhiladelph}
+          quantity={15}
+        />
+        </div>
       </section>
 
       {/* Mailbox з умовним рендером */}
@@ -88,10 +114,7 @@ function App() {
       </section>
 
       {/* Список книг з ключами */}
-      <section>
-        <h2>Our menu</h2>
-        <MenuDishes dishes={menuDishes} />
-      </section>
+     
 
       {/* Фрагменти */}
       
